c59d1359b16be61b5f34b062462948b378db23c5
Igor Piedade <igorpiedade@hotmail.com>
2023-07-27 06:02:14 -0400

    bug: :bug: fixing tech card icon

--- src/app/components/TechCard.tsx
+++ src/app/components/TechCard.tsx
@@ -9,7 +9,13 @@ export default function TechCard({ icon, caption }: Props) {
   return (
     <div className="flex h-36 w-36 flex-col overflow-clip rounded-b-3xl rounded-t-md bg-zinc-50 shadow-lg shadow-slate-400">
       <div className="flex h-28  w-full  place-items-center justify-center">
-        <Image src={icon} alt="" width={0} height={0} className="object-fill" />
+        <Image
+          src={icon}
+          alt=""
+          width={144}
+          height={144}
+          className="object-fill"
+        />
       </div>
       <label className="tex flex h-8 w-full place-items-center justify-center bg-zinc-800 text-lg text-zinc-100">
         {caption}
