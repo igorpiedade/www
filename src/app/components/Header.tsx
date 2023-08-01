73ef67df724d241bc645bd8b4a930459716c4d65
Igor Piedade <igorpiedade@hotmail.com>
2023-08-01 19:37:56 -0400

    style :art: Improving slider CSS and correcting some texts

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -4,7 +4,7 @@ import SocialNav from './SocialNav'
 
 export default function Header() {
   return (
-    <header className=" header fixed top-0 flex h-16 w-screen place-content-center overflow-hidden bg-gray-950 px-5 text-slate-300 bg-blend-color-burn">
+    <header className=" header fixed top-0 z-10 flex h-16 w-screen place-content-center overflow-hidden bg-gray-950 px-5 text-slate-300 bg-blend-color-burn">
       <div className="text-6 flex w-screen max-w-7xl items-center justify-between">
         <Logo />
         {/* <NavMenu /> */}
--- src/app/components/Profile.tsx
+++ src/app/components/Profile.tsx
@@ -5,27 +5,27 @@ export default function Profile() {
         <div className="m-10 w-1/3">
           <h1 className=" font-logo text-2xl ">Who am I?</h1>
           <p className=" font-navMenu">
-            I&#39;m a Brazilian guy with Russian first name, Portuguese last
-            name &#40; what a mix! &#41; living my lifelong dream of work and
-            live in USA. Technology is not only my job, but one of my passion.
-            I&#39;m very entusihastic with inovation, new tecnologies, robotics,
-            AI, Gadgets, home automation, and etc...
+            I&#39;m a Brazilian guy with a Russian first-name Portuguese last
+            name &#40; what a mix! &#41;, living my lifelong dream of working
+            and living in the USA. Technology is not only my job but one of my
+            passions. I&#39;m enthusiastic about innovation, new technologies,
+            robotics, AI, Gadgets, home automation, etc...
           </p>
         </div>
         <div className="m-10 w-1/3">
           <h1 className=" font-logo text-2xl">What I love</h1>
           <p className=" font-navMenu">
-            Besides of the technology field, I also love to travel, cook, cars,
-            books and of course, my life partner, my wife who I share all my
+            Besides the technology field, I also love to travel, cook, cars,
+            books, and of course, my life partner, my wife who I share all my
             best experiences.
           </p>
         </div>
         <div className="m-10 w-1/3">
           <h1 className=" font-logo text-2xl">The Professional</h1>
           <p className=" font-navMenu">
-            A novice full stack developer, who has been spending all his free
+            A novice full-stack developer who has been spending all his free
             time programming study projects and looking for opportunities to
-            start putting all this new knowledge in practice. I also have over
+            start putting all this new knowledge into practice. I also have over
             15 years experience as a network administrator and support. But
             that&#39;s another story.
           </p>
