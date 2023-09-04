fcf5d30f9780b78585652195dbdb05620ebc66cd
Igor Piedade <igorpiedade@hotmail.com>
2023-09-04 06:12:40 -0400

    feat :bento: Add new portifolio card for personal website project

--- src/app/components/Portifolio.tsx
+++ src/app/components/Portifolio.tsx
@@ -6,6 +6,7 @@ import spacetimeCard from '../../../public/assets/spacetime_card.png'
 import spacetimeMobileCard from '../../../public/assets/spacetimeMobile_card.png'
 import spacetimeAPICard from '../../../public/assets/spacetimeAPI_card.png'
 import notificationServiceCard from '../../../public/assets/notificationService_card.png'
+import personalwebCard from 'public/assets/personaIweb_card.png'
 
 type slidesProp = {
   id: number
@@ -18,24 +19,30 @@ export default function Portifolio() {
   const slidesArray: slidesProp = [
     {
       id: 1,
+      img: personalwebCard,
+      alt: 'a card image showing a screenshot of this website with a link to the github repo',
+      link: 'https://github.com/igorpiedade/www',
+    },
+    {
+      id: 2,
       img: notificationServiceCard,
       alt: 'a card image showing insominia app interating with the application',
       link: 'https://github.com/igorpiedade/notification-service',
     },
     {
-      id: 2,
+      id: 3,
       img: spacetimeCard,
       alt: 'a card image with a screenshot of the web application spacetime',
       link: 'https://github.com/igorpiedade/spacetime_web',
     },
     {
-      id: 3,
+      id: 4,
       img: spacetimeMobileCard,
       alt: 'a card with a sample image of the mobile aplication from the spacetime project',
       link: 'https://github.com/igorpiedade/spacetime_mobile',
     },
     {
-      id: 4,
+      id: 5,
       img: spacetimeAPICard,
       alt: 'a card image with a snippet code of the spacetime back-end',
       link: 'https://github.com/igorpiedade/spacetime_api',
